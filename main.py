530cd063f1a7c7c4952343a98aea7f798a39d92b
Elina-If <esiland@web.de>
2026-06-17 21:43:52 +0200

    Aufbau Struktur für Beziehungen zwischen Arbeitspaketen

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # FastAPI-Anwendung erstellen
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React App
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Endpunkt für die Root-URL
 @app.get("/")
